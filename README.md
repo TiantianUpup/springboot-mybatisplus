1c804b1b9f462be7acba56be04ccf6bf12dd6e09
unknown <hetiantian@spc-hz20121817.hikvision.com>
2019-08-02 17:33:48 +0800

    add service/dao/pojo

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SpringBoot整合MyBatisPlus框架
 
 ### Reference Documentation
+[SpringBoot整合MyBatis-plus](https://www.cnblogs.com/V1haoge/p/10125279.html)
 
 
---
+++ src/main/java/com/h2t/study/UserDAO.java
@@ -0,0 +1,13 @@
+package com.h2t.study;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * TODO Description
+ *
+ * @author hetiantian
+ * @version 1.0
+ * @Date 2019/08/02 16:44
+ */
+public interface UserDAO extends BaseMapper<User> {
+}
